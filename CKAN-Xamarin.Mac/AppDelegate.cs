e5b2835ce70e977dcc39eb479ad67c6b8757bc97
Joshua Brot <jbrot@umich.edu>
2019-06-23 13:26:15 -0500

    Creamte Xamarin and Xamarin.Mac project.

---
+++ CKAN-Xamarin.Mac/AppDelegate.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using AppKit;
+using Foundation;
+
+namespace CKAN_Xamarin.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
+    {
+
+        NSWindow window;
+
+        public AppDelegate ()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin.Forms on Mac!";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow {
+            get => window;
+        }
+
+
+        public override void DidFinishLaunching (NSNotification notification)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate (NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        public override bool ApplicationShouldHandleReopen (NSApplication sender, bool hasVisibleWindows)
+        {
+            if (hasVisibleWindows) {
+                window.OrderFront(this);
+            } else {
+                window.MakeKeyAndOrderFront(this);
+            }
+
+            return true;
+        }
+    }
+}
