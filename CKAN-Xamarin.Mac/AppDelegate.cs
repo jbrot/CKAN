78ee1213c9e288bba7782ab1d0d0470e5f71cb5b
Joshua Brot <jbrot@umich.edu>
2019-08-04 18:00:34 -0500

    Created Xamarin Forms project.

---
+++ CKAN-Xamarin.Mac/AppDelegate.cs
@@ -0,0 +1,45 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace CKAN.Xamarin.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled | NSWindowStyle.Miniaturizable;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow { get => window; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override bool ApplicationShouldHandleReopen(NSApplication sender, bool hasVisibleWindows)
+        {
+            if (hasVisibleWindows)
+            {
+                window.OrderFront(this);
+            }
+            else
+            {
+                window.MakeKeyAndOrderFront(this);
+            }
+
+            return true;
+        }
+    }
+}
