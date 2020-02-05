675e51e4a62f32b827f5d71a362c1a1afaa220c0
Joshua Brot <jbrot@umich.edu>
2020-02-05 16:13:29 -0500

    Created MasterPage ViewModel and moved items into it.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

---
+++ Xamarin/ViewModel/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CKAN.Xamarin.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BaseViewModel ()
+        {
+        }
+
+        protected bool SetProperty<T> (ref T field, T value, [CallerMemberName]string name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(name);
+            return true;
+        }
+
+        protected void OnPropertyChanged ([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Xamarin/ViewModel/MasterPageModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using CKAN.Xamarin.Model;
+using CKAN.Xamarin.View;
+using Xamarin.Forms;
+
+namespace CKAN.Xamarin.ViewModel
+{
+    public class MasterPageModel : BaseViewModel
+    {
+        private IList<MasterPageItem> items;
+        public IList<MasterPageItem> Items {
+            get { return items; }
+            set { SetProperty(ref items, value); }
+        }
+
+        public MasterPageModel ()
+        {
+            // TODO: Put in proper icons
+            // TODO: Re: discussion in MasterPageItem: Referring to the *Page types
+            // here violates MVVM. This suggests that this list should be in the View
+            // not the ViewModel, which also feels wrong. This seems to suggest to
+            // me that the Enum approach is definitely the way to go.
+            Items = new List<MasterPageItem> {
+                new MasterPageItem {
+                    Label = "Search",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = typeof(SearchPage)
+                },
+                new MasterPageItem {
+                    Label = "Browse",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = typeof(BrowsePage)
+                },
+                new MasterPageItem {
+                    Label = "Installed",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = typeof(InstalledPage)
+                },
+                new MasterPageItem {
+                    Label = "Updates",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = typeof(UpdatesPage)
+                },
+                new MasterPageItem {
+                    Label = "Settings",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = typeof(SettingsPage)
+                },
+                new MasterPageItem {
+                    Label = "Run",
+                    Icon = ImageSource.FromFile("Logo"),
+                    ContentType = null
+                }
+            };
+        }
+    }
+}
