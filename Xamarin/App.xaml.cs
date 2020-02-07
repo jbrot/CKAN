279886c1ef106afb15de2c57af000a03c70ab574
Joshua Brot <jbrot@umich.edu>
2020-02-07 16:30:31 -0500

    Created CkanService.
    Time to start configuring CKAN!
    
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

--- Xamarin/App.xaml.cs
+++ Xamarin/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using Autofac;
+using CKAN.Xamarin.Service;
 using CKAN.Xamarin.View;
 using CKAN.Xamarin.ViewModel;
 using Xamarin.Forms;
@@ -42,22 +43,38 @@ namespace CKAN.Xamarin
 
             guiScope = ServiceLocator.Container.BeginLifetimeScope(builder => {
                 var asm = Assembly.GetExecutingAssembly();
+                // Register subclasses of BaseViewModel
                 builder.RegisterAssemblyTypes(asm)
                        .Where(t => t.IsSubclassOf(typeof(BaseViewModel)))
                        .InstancePerLifetimeScope();
+                // Register implementers of IMvvmView<>
                 builder.RegisterAssemblyTypes(asm)
                        .Where(t => t.GetInterfaces().Any(x =>
                             x.IsGenericType &&
                             x.GetGenericTypeDefinition() == typeof(IMvvmView<>)))
                        .AsImplementedInterfaces()
                        .InstancePerLifetimeScope();
-            });
+                // Register services as singletons
+                builder.RegisterAssemblyTypes(asm)
+                       .Where(t => t.Name.EndsWith("Service"))
+                       .SingleInstance();
+                // If we have a KSPManager, register it so it will get passed
+                // to CkanService. Otherwise, let CkanService construct its own.
+                if (mgr != null) {
+                    // We register KSPManager as externally owned so that
+                    // CkanService can call its dispose in all situations.
+                    builder.RegisterInstance(mgr)
+                           .As<KSPManager>().ExternallyOwned();
+                }
 
-            Manager = mgr ?? new KSPManager(new NullUser());
+            });
 
             // Resolve the main pages in the outer LifetimeScope, as we
             // only need one of each.
             MainPage = (Page) guiScope.Resolve<IMvvmView<MainPageViewModel>>();
+
+            // Start loading Ckan
+            guiScope.Resolve<CkanService>().Init();
         }
 
         ~App ()
---
+++ Xamarin/Service/CkanService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CKAN.Xamarin.Service
+{
+    /// <summary>
+    /// The CkanService is responsible for setting up CKAN (i.e., the CKAN-core
+    /// project) and presenting data from it to the GUI.
+    ///
+    /// <br />
+    ///
+    /// Classes which end with Service are automatically instantiated as
+    /// singletons by Autofac (this is set up in App).
+    /// </summary>
+    public class CkanService : IDisposable
+    {
+        private KSPManager Manager;
+
+        /// <summary>
+        /// Autofac will use this constructor if no KSPManager is registered.
+        /// This means we need to create our own.
+        /// </summary>
+        public CkanService()
+        { }
+
+        /// <summary>
+        /// Autofac will use this constructor if a KSPManager is registered.
+        /// In this case, we can jump straight to instance configuration.
+        /// </summary>
+        /// <param name="manager"></param>
+        public CkanService (KSPManager manager)
+        {
+            manager = Manager;
+        }
+
+        public void Dispose ()
+        {
+            Manager.Dispose();
+        }
+
+        /// <summary>
+        /// This method will start asynchronously initializing CKAN. As things
+        /// progress, the public properties will be updated (on the UI thread).
+        /// This means that you can bind to these values and automatically
+        /// display the correct information once it has loaded.
+        /// </summary>
+        public void Init()
+        {
+            Task.Run(async () => {
+                if (Manager == null) {
+                    var mgr = new KSPManager(new NullUser());
+                    await Device.InvokeOnMainThreadAsync(() => Manager = mgr);
+                }
+            });
+        }
+    }
+}
