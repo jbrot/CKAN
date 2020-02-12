2ad8e280d008b962413fa71c801fa9dfda8d2319
Joshua Brot <jbrot@umich.edu>
2020-02-12 13:34:59 -0500

    Fleshed out more of the settings GUI.
    Up next: implement KSP instance editing.
    
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

---
+++ Xamarin/View/EditableList.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CKAN.Xamarin.View
+{
+    public partial class EditableList : ContentView
+    {
+        public EditableList ()
+        {
+            InitializeComponent();
+        }
+    }
+}
